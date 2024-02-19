bfdea67e0b4cb71de342d9d36842b67d9e8b3dc8
Abhay <vickysahgal1998@gmial.com>
2024-02-19 09:10:53 +0530

    First Commit

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -10,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init()
+    }
+    public void init(){
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        btn5=findViewById(R.id.btn5);
+        btn6=findViewById(R.id.btn6);
+        btn7=findViewById(R.id.btn7);
+        btn8=findViewById(R.id.btn8);
+        btn9=findViewById(R.id.btn9);
     }
 }
