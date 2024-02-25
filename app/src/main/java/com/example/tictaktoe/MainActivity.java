beea1d27a527055d0b932e8a80b6fe8e744b72f2
Abhay <vickysahgal1998@gmial.com>
2024-02-25 08:16:11 +0530

    5th Commit

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -64,28 +64,60 @@ String b1,b2,b3,b4,b5,b6,b7,b8,b9;
             if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")) {
 //            1
                 Toast.makeText(this, "winner is:" + b1, Toast.LENGTH_SHORT).show();
+                newGame();
             } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
                 //2
-                Toast.makeText(this, "winner is:" + b4, Toast.LENGTH_SHORT).show();
+                 Toast.makeText(this, "winner is:" + b4, Toast.LENGTH_SHORT).show();
+                 newGame();
             } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
                 //3
                 Toast.makeText(this, "winner is:" + b7, Toast.LENGTH_SHORT).show();
+                newGame();
             } else if (b1.equals(b4) && b8.equals(b7) && !b1.equals("")) {
                 //4
                 Toast.makeText(this, "Winner is:" + b1, Toast.LENGTH_SHORT).show();
+                newGame();
             } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
                 //5
                 Toast.makeText(this, "winner is:" + b2, Toast.LENGTH_SHORT).show();
+                newGame();
             } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
                 //6
                 Toast.makeText(this, "winner is:" + b3, Toast.LENGTH_SHORT).show();
+                newGame();
             } else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
                 //7
                 Toast.makeText(this, "Winner is:" + b1, Toast.LENGTH_SHORT).show();
+                newGame();
             } else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
                 //8
                 Toast.makeText(this, "Winner is:" + b3, Toast.LENGTH_SHORT).show();
+                newGame();
+            } else if (!b1.equals("") && !b2.equals("") && !b3.equals("")
+                        && !b4.equals("") && !b5.equals("") && !b6.equals("")
+                       && !b7.equals("") && !b8.equals("") && !b9.equals("")) {
+                newGame();
             }
         }
     }
+
+    public void newGame(){
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+        btn4.setText("");
+        btn5.setText("");
+        btn6.setText("");
+        btn7.setText("");
+        btn8.setText("");
+        btn9.setText("");
+        count=0;
+        flag=0;
+
+    }
+
+
+
+
+
 }
