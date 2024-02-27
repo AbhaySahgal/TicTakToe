a661dd9a76f9d1c6b01b4472f366342bbdd0f739
Abhay <vickysahgal1998@gmial.com>
2024-02-27 08:15:57 +0530

    Intent passing done from MainActivity1.java to MainActivity.java

--- app/src/main/java/com/example/tictaktoe/MainActivity1.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity1.java
@@ -2,7 +2,10 @@ package com.example.tictaktoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity1 extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+
+        Button btn;
+        btn= findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inext;
+                inext=new Intent(MainActivity1.this, MainActivity.class);
+                startActivity(inext);
+            }
+        });
     }
 }
